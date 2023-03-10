5d2ef8048b256264f0343ef9730ebb319718fe36
vasyl <vasilijpodlevskij@gmail.com>
2023-03-10 23:14:48 +0100

    druga ciesc

--- main.js
+++ main.js
@@ -1,33 +1,74 @@
 const box = document.querySelector('.box');
 const container = document.querySelector('.container');
+const pointOff = document.querySelector('.point-off');
+const btn = document.querySelector('.btn');
+const start = document.querySelector('.start');
+const btnStart = document.createElement('button');
 
-const pointA = document.querySelector('.pointA');
-const pointB = document.querySelector('.pointB');
 const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 let index = 0;
-    
+let idname = 1;
+let inStPo = 0;
+let canNot = true   
+let flag = true
 
-const btn = document.querySelector('.btn');
+pointOff.addEventListener('click', () => {
+    canNot = false;
+    inStPo = 0
+    container.addEventListener('mousedown', (e) => {
+        const x = e.offsetX
+        const y = e.offsetY
+        
+        if(inStPo >= 1) return;
+        pointOff.style.display = 'none'
+        const startPoint = document.createElement('div');
+        container.appendChild(startPoint);
+        startPoint.classList.add('point');
+        startPoint.setAttribute('id',`${inStPo}`);
+        startPoint.textContent = 'First point'
+        startPoint.style.left = `${x}px`;
+        startPoint.style.top =  `${y}px`;
+        inStPo++
+        
+        box.appendChild(btnStart);
+        btnStart.classList.add('btnStyle')
+        // btnStart.classList.add('start')
+        btnStart.textContent = "Start"
+    })
+})
 
-let flag = false
+
+
 container.addEventListener('mousedown', (e) => {
+    if(canNot == false) return;
+    pointOff.style.display = 'inline-block'
+
     const x = e.offsetX
-    const y = e.offsetY 
-    console.log(index);
+    const y = e.offsetY
+
     if(index == letters.length){
         index = 0
     }
+
     const node = document.createElement('div')
     container.appendChild(node)
     node.classList.add('point');
+    node.setAttribute('id',`${idname++}`)
     node.textContent = letters[index]
     node.style.left = `${x}px`;
     node.style.top =  `${y}px`;
-    index++
+    index++ 
+    console.log(pointOff.style.display);
 })
+
+
 
 btn.addEventListener('click', () => {
     container.textContent = ''
-    index = 0
+    index = 0;
+    idname = 1;
+    pointOff.style.display = 'inline-block'
+    canNot = true
+    box.removeChild(btnStart)
 })
 
